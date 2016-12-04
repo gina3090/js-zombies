a8cc9d4754b4b62a6a679720ccd98c291f14e5d0
ginagala3090 <ginagala3090@users.noreply.github.com>
2016-12-04 10:54:41 -1000

    created eat function

--- zombies.js
+++ zombies.js
@@ -236,7 +236,16 @@ equip(itemToEquip) {
  * @name eat
  * @param {Food} itemToEat  The food item to eat.
  */
-
+eat(itemToEat) {
+  if(itemToEat instanceof Food && this._pack.indexOf(itemToEat) > -1) {
+    this._pack.splice(this._pack.indexOf(itemToEat), 1);
+    if(this.health + itemToEat.energy < this._maxHealth) {
+      this.health += itemToEat.energy;
+    } else {
+      this.health = this._maxHealth;
+    }
+  }
+}
 
 /**
  * Player Class Method => useItem(item)
