dc459c017c1c22c48c1acf9dfaed3a61b56cfb9e
ginagala3090 <ginagala3090@users.noreply.github.com>
2016-12-03 16:19:21 -1000

    created takeItem function

--- zombies.js
+++ zombies.js
@@ -138,7 +138,16 @@ class Player {
  * @param {Item/Weapon/Food} item   The item to take.
  * @return {boolean} true/false     Whether player was able to store item in pack.
  */
-
+takeItem(item) {
+  if(this._pack.length <= 2) {
+    this._pack.push(item);
+    console.log(Player.name + ' picked up a ' + item + '.');
+    return true;
+  } else {
+    console.log(Player.name + '\'s pack is full so ' + item + ' cannot be stored.');
+    return false;
+  }
+}
 
 /**
  * Player Class Method => discardItem(item)
